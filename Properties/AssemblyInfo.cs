f096a99a1df766f1f973c78d4119bdde5804cec3
oozcitak <oozcitak@47f3aa98-cfd1-11de-800d-65735d9f770d>
2010-08-23 07:56:21 +0000

    Fixes Issue 23.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,6 +14,11 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2009")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// The AllowPartiallyTrustedCallersAttribute requires the assembly to be signed with a strong name key.
+// This attribute is necessary since the control is called by either an intranet or Internet
+// Web page that should be running under restricted permissions.
+[assembly: AllowPartiallyTrustedCallers()]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 

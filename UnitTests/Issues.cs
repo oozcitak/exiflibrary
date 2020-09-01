5a5da968e8af1eeade37c848a407243ca9896b5b
Özgür Özçıtak <ozcitak@yahoo.com>
2020-09-01 10:56:31 +0300

    Add test for #66. Fixes #66.

--- UnitTests/Issues.cs
+++ UnitTests/Issues.cs
@@ -1,11 +1,42 @@
 using System;
 using Xunit;
 using ExifLibrary;
+using System.Linq;
 
 namespace UnitTests
 {
     public class Issues
     {
+        [Fact(DisplayName = "An item with the same key has already been added.")]
+        public void Issue66()
+        {
+            var originalDateTime = new DateTime(2019, 12, 07, 19, 34, 35, DateTimeKind.Local);
+            var modifiedDateTime = new DateTime(2019, 11, 07, 19, 34, 35, DateTimeKind.Local);
+
+            var srcImage = TestHelpers.TestImagePath(".", "issue-66.jpg");
+            var dstImage = TestHelpers.TestImagePath(".", "issue-66e.jpg");
+            var file = ImageFile.FromFile(srcImage);
+
+            var dateTimeProperty = file.Properties.FirstOrDefault(p => p.Name == "DateTime");
+
+            if (dateTimeProperty?.Value is DateTime dt)
+            {
+                if (dt.Date == originalDateTime.Date)
+                {
+                    dateTimeProperty.Value = modifiedDateTime;
+                }
+                else
+                {
+                    dateTimeProperty.Value = originalDateTime;
+                }
+            }
+
+            ;
+
+            var exception = Record.Exception(() => file.Save(dstImage));
+            Assert.Null(exception);
+        }
+
         [Fact(DisplayName = "Exceptions when trying to set value of type UFraction32")]
         public void Issue77()
         {
